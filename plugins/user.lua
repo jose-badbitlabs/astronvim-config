3f922245196ff52a65c4455675a78129524957d0
Jose Calles <jose@badbitlabs.com>
2023-03-16 16:37:25 -0400

    install additional plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,18 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    {
+        "ray-x/go.nvim",
+        dependencies = {"ray-x/guihua.lua"},
+        config = function() require("go").setup() end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()'
+    }, {
+        "windwp/nvim-spectre",
+        config = function() require("spectre").setup() end,
+        event = {"CmdlineEnter"}
+    }, {
+        "kylechui/nvim-surround",
+        config = function() require("nvim-surround").setup() end,
+        event = {"BufEnter"}
+    }
 }
