e4c7c2269388bc76d056ff9eca470881224f692f
Jose Calles <jose@badbitlabs.com>
2023-03-22 14:48:59 -0400

    reformat using stylelua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,14 @@
 return {
-    -- Add the community repository of plugin specifications
-    "AstroNvim/astrocommunity",
-    -- example of imporing a plugin, comment out to use it or add your own
-    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-    {import = "astrocommunity.colorscheme.nightfox"},
-    {import = "astrocommunity.pack.go"}
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
 }
