02144ef75df2b6b3652f1c5cf60b5a70ae3cd48c
Jose Calles <jose@badbitlabs.com>
2023-03-31 14:04:27 -0400

    update config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.json" },
@@ -12,4 +15,5 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.project.nvim-spectre" },
 }
