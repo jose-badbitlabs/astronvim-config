f6ebbb18cf1d5d7d287cca4f2ae1143f3163f522
Jose Calles <jose@badbitlabs.com>
2023-04-01 18:12:25 -0400

    change theme to nightfox

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "terafox",
+  colorscheme = "nightfox",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = { virtual_text = true, underline = true },
   lsp = {
